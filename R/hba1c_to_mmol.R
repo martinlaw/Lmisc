f68a1bd2bd74e02fc2a6eff0e0b4635db1cf69ea
Martin Law <martin.law@mrc-bsu.cam.ac.uk>
2026-04-10 18:33:18 +0100

    update hba1c fns to cope with std dev.s

--- R/hba1c_to_mmol.R
+++ R/hba1c_to_mmol.R
@@ -1,13 +1,14 @@
 #' Converts HbA1c from percentage to mmol per mol
 #'
 #' @param percent HbA1c value to convert
+#' @param sd Logical: is the value a standard deviation? Default FALSE.
 #'
 #' @returns HbA1c in mmol per mol
 #' @export
 #'
 #' @examples
 #' hba1c_to_mmol(percent=7)
-hba1c_to_mmol <- function(percent){
-  mmol <- 10.929*(percent-2.152)
+hba1c_to_mmol <- function(percent, sd=FALSE){
+  mmol <- ifelse(test=sd==TRUE, yes=10.929*percent, no=10.929*(percent-2.152))
   mmol
 }
--- R/hba1c_to_percent.R
+++ R/hba1c_to_percent.R
@@ -1,14 +1,15 @@
-#' Converts HbA1c from mmol per mol to perecentage
+#' Converts HbA1c from mmol per mol to percentage
 #'
-#' @param percent HbA1c value to convert
-#'
+#' @param mmol HbA1c value to convert
+#' @param sd Logical: is the value a standard deviation? Default FALSE.
+
 #' @returns HbA1c in percentage
 #' @export
 #'
 #' @examples
 #' hba1c_to_percent(mmol=47)
-hba1c_to_percent <- function(mmol){
-  percent <- (mmol/10.9292)+2.152
+hba1c_to_percent <- function(mmol, sd=FALSE){
+  percent <- ifelse(test=sd==TRUE, yes=(mmol/10.9292), no=(mmol/10.9292)+2.152)
   percent
 }
 
