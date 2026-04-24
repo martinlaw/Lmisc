7fffd51429f4682b2279c5478daf3053a36c21ea
Martin Law <martin.law@mrc-bsu.cam.ac.uk>
2026-04-24 16:51:57 +0100

    updated set_path to append "data" subfolder by default

--- R/set_path.R
+++ R/set_path.R
@@ -1,14 +1,16 @@
-#' Create character for path to project
+#' Create character for path to project data
 #'
 #' @param project.number Project number
+#' @param data Logical: Append "data" subfolder? Default TRUE.
 #'
 #' @returns Character for path to project.
 #'
 #' @export
 #' @examples
 #' set_path(135)
-set_path <- function(project.number){
-  common.path <-  "//bliss-04-nfs.mrc-bsu.cam.ac.uk/projects"
+set_path <- function(project.number, data=TRUE){
+  common.path <- "//bliss-04-nfs.mrc-bsu.cam.ac.uk/projects"
   project.path <- file.path(common.path, paste0("PTUC", project.number))
+  if(data==TRUE) project.path <- file.path(project.path, "data")
   project.path
 }
