cefc3b54081f010b6926b6a01ad3b203f8eabbc7
Martin Law <martin.law@mrc-bsu.cam.ac.uk>
2026-04-24 17:01:56 +0100

    bug fix

--- R/set_path.R
+++ R/set_path.R
@@ -10,7 +10,7 @@
 #' set_path(135)
 set_path <- function(project.number, data=TRUE){
   common.path <- "//bliss-04-nfs.mrc-bsu.cam.ac.uk/projects"
-  project.path <- file.path(common.path, paste0("PTUC", project.number))
+  project.path <- file.path(common.path, "PTUC", project.number)
   if(data==TRUE) project.path <- file.path(project.path, "data")
   project.path
 }
